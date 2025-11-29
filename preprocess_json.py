add80a306e1e1e2fc37281225575a75b8890c1e6
Shivanigupta03 <shivanigupta180702@gmail.com>
2025-11-29 15:35:48 +0530

    Update preprocess_json.py

--- preprocess_json.py
+++ preprocess_json.py
@@ -7,7 +7,6 @@ from sklearn.metrics.pairwise import cosine_similarity
 import joblib
 
 def create_embedding(text_list):
-    # https://github.com/ollama/ollama/blob/main/docs/api.md#generate-embeddings
     r = requests.post("http://localhost:11434/api/embed", json={
         "model": "bge-m3",
         "input": text_list
@@ -34,6 +33,5 @@ for json_file in jsons:
         my_dicts.append(chunk) 
 
 df = pd.DataFrame.from_records(my_dicts)
-# Save this dataframe
 joblib.dump(df, 'embeddings.joblib')
 
